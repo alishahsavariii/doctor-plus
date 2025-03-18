823d6a7ea40116e50777e3c6a364cfbe5308c677
alishahsavariii <ali_shahsavari1996@yahoo.com>
2025-03-18 12:37:00 +0330

    [INIT] add vazirmatn font

---
+++ doctor-plus/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Vazirmatn } from "next/font/google";
+import "./globals.css";
+
+const vazirmatn = Vazirmatn({
+  subsets: ["latin", "arabic"],
+  display: "swap"
+})
+
+export const metadata: Metadata = {
+  title: "دکتر پلاس",
+  description: "سامانه جستجو آنلاین دکتر و رزرو آنلاین",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fa" dir="rtl" className={vazirmatn.className}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
